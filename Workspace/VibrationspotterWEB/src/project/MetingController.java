d00a9b78502f3d32365a485a09a5b4249b938513
thomas.bruneel <thomas.bruneel@hotmail.com>
2017-11-19 19:15:35 +0100

    JSF page en backing bean verder afgewerkt.

---
+++ Workspace/VibrationspotterWEB/src/project/MetingController.java
@@ -0,0 +1,37 @@
+package project;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+//http://localhost:8080/testWEB/
+@ManagedBean
+@RequestScoped
+public class MetingController {
+
+  private String output="alle velden invullen";
+  private Meting m=new Meting();
+
+  public String submit() {
+		output = m.getUser()+", uw project met titel "+m.getTitel()+" werd met succes aangemaakt!";
+	//EJB aanroepen
+    setM(null);
+    return "index.xhtml";
+  }
+
+
+  public String getOutput() {
+    return output;
+  }
+
+  public void setOutput(String output) {
+    this.output = output;
+  }
+
+public Meting getM() {
+	return m;
+}
+
+public void setM(Meting m) {
+	this.m = m;
+}
+
+}
