ceb17f3867564a7b674d26dbba44f2cf4fd03a56
Pieterism <pervdh@gmail.com>
2017-11-22 13:08:36 +0100

    MetingManagementEJB aangepast

--- Workspace/VibrationspotterEJB/ejbModule/Vibrationspotter/MetingManagementEJB.java
+++ Workspace/VibrationspotterEJB/ejbModule/Vibrationspotter/MetingManagementEJB.java
@@ -1,5 +1,19 @@
 package Vibrationspotter;
 
-public class MetingManagementEJB {
+import model.Meting;
+
+public class MetingManagementEJB implements MetingManagementEJBLocal{
+
+	@Override
+	public Meting findMeting(String titelMeting) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addMeting(String gebruikersnaam, String titel, String Locatie, int x, int y, int z) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
