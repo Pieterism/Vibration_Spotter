a0edf1ae248b12bb14d82406db66863d33da2233
Pieterism <pervdh@gmail.com>
2017-11-22 13:09:33 +0100

    MetingManagementEJB aangepast for real

--- Workspace/VibrationspotterEJB/ejbModule/Vibrationspotter/MetingManagementEJB.java
+++ Workspace/VibrationspotterEJB/ejbModule/Vibrationspotter/MetingManagementEJB.java
@@ -1,9 +1,24 @@
 package Vibrationspotter;
 
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.ejb.SessionContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import model.Meting;
 
 public class MetingManagementEJB implements MetingManagementEJBLocal{
+	
+	@PersistenceContext(unitName="vibrationspotter")
+	private EntityManager em;
 
+	@EJB
+	private PersonManagementEJBLocal personEJB;
+
+	@Resource
+	private SessionContext ctx;
+	
 	@Override
 	public Meting findMeting(String titelMeting) {
 		// TODO Auto-generated method stub
