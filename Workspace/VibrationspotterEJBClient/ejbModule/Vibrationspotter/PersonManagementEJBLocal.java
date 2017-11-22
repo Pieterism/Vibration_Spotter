9f7f4780edcbd97a276d18d327bc2e65aedfb9a7
Pieterism <pervdh@gmail.com>
2017-11-22 13:24:33 +0100

    update

--- Workspace/VibrationspotterEJBClient/ejbModule/Vibrationspotter/PersonManagementEJBLocal.java
+++ Workspace/VibrationspotterEJBClient/ejbModule/Vibrationspotter/PersonManagementEJBLocal.java
@@ -1,12 +1,5 @@
 package Vibrationspotter;
 
-import javax.ejb.Local;
-
-import model.Persoon;
-
-@Local
 public interface PersonManagementEJBLocal {
-	public Persoon findPerson(String login);
-	
-	public void addPerson(String Person, String Password);
+
 }
