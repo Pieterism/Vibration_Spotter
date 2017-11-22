8505fcc7ac1962a2b008b81e94e679a21a885e7b
Pieterism <pervdh@gmail.com>
2017-11-22 12:39:53 +0100

    EJBClientLocal

--- Workspace/VibrationspotterEJBClient/ejbModule/Vibrationspotter/MetingManagementEJBLocal.java
+++ Workspace/VibrationspotterEJBClient/ejbModule/Vibrationspotter/MetingManagementEJBLocal.java
@@ -1,5 +1,12 @@
 package Vibrationspotter;
 
-public interface MetingManagementEJBLocal {
+import javax.ejb.Local;
 
+import model.Meting;
+
+@Local
+public interface MetingManagementEJBLocal {
+	public Meting findMeting(String titelMeting);
+	
+	public void addMeting(String gebruikersnaam, String titel, String Locatie, int x, int y, int z);
 }
