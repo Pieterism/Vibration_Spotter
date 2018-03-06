45b2de80e8fd9dc1043daf68d29828b8e67b6faa
thomas.bruneel <thomas.bruneel@hotmail.com>
2018-03-06 10:10:16 +0100

    projectcontroller aangepast

--- Workspace/VibrationspotterWEB/src/project/ProjectController.java
+++ Workspace/VibrationspotterWEB/src/project/ProjectController.java
@@ -1,5 +1,30 @@
 package project;
 
-public class ProjectController {
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import Vibrationspotter.MetingManagementEJBLocal;
+import Vibrationspotter.ProjectManagementEJBLocal;
+import model.Project;
+
+@Named
+@ViewScoped
+public class ProjectController implements Serializable {
+
+private static final long serialVersionUID = 1L;
+
+@EJB
+private ProjectManagementEJBLocal projectejb;
+
+private Project project=new Project();
+
+
+
+public void submit() {
+projectejb.addProject(project);
+}
 
 }
