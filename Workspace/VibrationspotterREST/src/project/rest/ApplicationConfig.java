7e714f8697a9ae4c9aa09cb0b12d2edfc49b4331
Birgen Vermang <birgen.vermang@gmail.com>
2018-03-11 17:43:04 +0100

    Twerkt nog niet :(

---
+++ Workspace/VibrationspotterREST/src/project/rest/ApplicationConfig.java
@@ -0,0 +1,30 @@
+package project.rest;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import com.owlike.genson.ext.jaxrs.GensonJsonConverter;
+
+@ApplicationPath("Restservice")
+public class ApplicationConfig extends Application{
+private final Set<Class<?>> classes;
+	
+	final ResourceConfig config = new ResourceConfig().register(GensonJsonConverter.class);
+
+	public ApplicationConfig() {
+		HashSet<Class<?>> c = new HashSet<>();
+		c.add(RestTest.class);
+		classes = Collections.unmodifiableSet(c);
+	}
+	
+	@Override
+	public Set<Class<?>> getClasses() {
+		return classes;
+	}
+}
