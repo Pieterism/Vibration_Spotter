da86f45f4f052bca8ba8cadf3f11911c330f9213
Pieterism <pervdh@gmail.com>
2018-03-13 10:45:45 +0100

    fixed password check

--- Workspace/VibrationspotterEJB/ejbModule/Vibrationspotter/LoginManagementEJB.java
+++ Workspace/VibrationspotterEJB/ejbModule/Vibrationspotter/LoginManagementEJB.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.xml.bind.DatatypeConverter;
 
+import Bcrypt.BCrypt;
+
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -16,40 +18,26 @@ import java.util.List;
 import model.Leerkracht;
 import model.Persoon;
 
-
 @Stateless
 public class LoginManagementEJB implements LoginManagementEJBLocal {
-	
-		@PersistenceContext(unitName="demodb")		
-		private EntityManager em;
-		
-		@Resource
-		private SessionContext ctx;
-		private BCrypt bcrypt;
 
-		
-		public boolean controleerpaswoord (String emailadres, String pwd) {
-			Query q = em.createQuery("SELECT p FROM Persoon p WHERE p.emailadres = :emailadres" );
-			q.setParameter("emailadres", emailadres);
-				List<Persoon> personen = q.getResultList();
-				System.out.println(personen.get(0).getPaswoord());
-				/*
-				if(personen.size() == 1){
-					if(pwd.equals(bcrypt.checkpw(pwd,personen.get(0).getPaswoord()))){
-						return true;		
-						
-					}
-					else{
-						return false;
-					}
-					}
-					*/
-				
+	@PersistenceContext(unitName = "demodb")
+	private EntityManager em;
 
-				
-			return false ;
+	@Resource
+	private SessionContext ctx;
+	private BCrypt bcrypt;
+
+	public boolean controleerpaswoord(String emailadres, String pwd) {
+		Query q = em.createQuery("SELECT p FROM Persoon p WHERE p.emailadres = :emailadres");
+		q.setParameter("emailadres", emailadres);
+		List<Persoon> personen = q.getResultList();
+		System.out.println(personen.get(0).getPaswoord());
+
+		if (personen.size() == 0) {
+			System.out.println("ERROR! Gebruiker bestaat niet! ");
+
 		}
+		return bcrypt.checkpw(pwd, personen.get(0).getPaswoord());
+	}
 }
-
-
-
