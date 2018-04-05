15962484fa40119327ebcb774bbb6038ae36adaa
Pieter-Jan Vanhaverbeke <pieter-jan.vanhaverbeke@student.kuleuven.be>
2018-04-05 17:53:47 +0200

    update registratieREST

--- Workspace/VibrationspotterREST/src/project/rest/RegistratieREST.java
+++ Workspace/VibrationspotterREST/src/project/rest/RegistratieREST.java
@@ -1,17 +1,26 @@
 package project.rest;
 
+import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
+import Vibrationspotter.SpotterManagementEJBLocal;
+
 @Path("Registreren")
 public class RegistratieREST {
+
+	@EJB
+	private SpotterManagementEJBLocal spotterEJB;
 	
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON})
 	public String registreren(String gegevens){
 		System.out.println(gegevens);
+		
+		spotterEJB.aanmakenSpotter(gegevens);
+		
 		return "[{\"Registratie\": Gelukt!!!}]";
 	}
 }
