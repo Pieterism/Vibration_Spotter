9e0fda608a443d4853ebddaa3190c05699a80613
Birgen Vermang <birgen.vermang@gmail.com>
2018-04-05 21:08:50 +0200

    EINDELIJK DIE NULLPOINTEREXCEPTION ERUIT

--- Workspace/VibrationspotterREST/src/project/rest/RegistratieREST.java
+++ Workspace/VibrationspotterREST/src/project/rest/RegistratieREST.java
@@ -1,6 +1,10 @@
 package project.rest;
 
+import java.io.Serializable;
+
 import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -8,8 +12,12 @@ import javax.ws.rs.core.MediaType;
 
 import Vibrationspotter.SpotterManagementEJBLocal;
 
+@Named
+@ViewScoped
 @Path("Registreren")
-public class RegistratieREST {
+public class RegistratieREST implements Serializable {
+ 
+	private static final long serialVersionUID = 1L;
 
 	@EJB
 	private SpotterManagementEJBLocal spotterEJB;
@@ -18,6 +26,11 @@ public class RegistratieREST {
 	@Consumes({MediaType.APPLICATION_JSON})
 	public String registreren(String gegevens){
 		System.out.println(gegevens);
+		if(spotterEJB == null) System.out.println("NULLL!");
+		
+		else{
+			System.out.println("het werkt");
+		}
 		
 		spotterEJB.aanmakenSpotter(gegevens);
 		
