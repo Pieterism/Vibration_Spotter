015ec56a3ee8ed0d4d2a593749dd6e9db6059909
Pieter-Jan Vanhaverbeke <pieter-jan.vanhaverbeke@student.kuleuven.be>
2018-04-16 12:54:26 +0200

    Update checken Login_Met_QR

--- Workspace/VibrationspotterREST/src/project/rest/QRcodeREST.java
+++ Workspace/VibrationspotterREST/src/project/rest/QRcodeREST.java
@@ -1,18 +1,39 @@
 package project.rest;
 
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
+import Vibrationspotter.ProjectManagementEJBLocal;
+
 @Path("QR")
-public class QRcodeREST {
+public class QRcodeREST implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private ProjectManagementEJBLocal projectEJB;
 	
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON})
-	public String qr_accept(String qr){
+	public String qr_accept(String qr) throws IOException{
+		String waarde;
+		
+		if(projectEJB.checkQR(qr)==true){
+			waarde = "true";
+		}
+		else{
+			waarde = "false";
+		}
+		
+		
 		System.out.println(qr);
-		return "[{\"Status\": Gelukt!!!}]";
+		return "waarde";
 	}
 	
 }
