3c0ee0611eeb908ad5fb8ee22924e8ba1bc8d8f0
Birgen Vermang <birgen.vermang@gmail.com>
2018-04-19 14:24:22 +0200

    Backup

--- APP/app/src/main/java/vibrationspotter/vibrationspotterapp/Splash.java
+++ APP/app/src/main/java/vibrationspotter/vibrationspotterapp/Splash.java
@@ -1,8 +1,11 @@
 package vibrationspotter.vibrationspotterapp;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
@@ -10,9 +13,40 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = (TextView) findViewById(R.id.textView);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/aclonica.regular.ttf");
-        textView.setTypeface(typeface);
         setContentView(R.layout.activity_splash);
+
+        //Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/aclonica.regular.ttf");
+        //textView.setTypeface(typeface);
+        //PIETER FIX PLZ
+
+        TextView registreren = (TextView) findViewById(R.id.link_signup);
+        Button schoolbutton = findViewById(R.id.scholenlogin);
+        Button gebruikerslogin = findViewById(R.id.gebruikerslogin);
+
+        schoolbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intetentSchool = new Intent(Splash.super.getApplicationContext(),LeerkrachtLogin.class);
+                startActivity(intetentSchool);
+            }
+        });
+
+        gebruikerslogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentGebruiker = new Intent(Splash.super.getApplicationContext(), GebruikerLogin.class);
+                startActivity(intentGebruiker);
+            }
+        });
+
+        registreren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentRegistreren = new Intent(Splash.super.getApplicationContext(), Registreren.class);
+                startActivity(intentRegistreren);
+            }
+        });
+
+
     }
 }
