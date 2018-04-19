56cbdaea366635506e79c1d9c05a2e0c84dc2d5f
Pieterism <pervdh@gmail.com>
2018-04-19 19:56:26 +0200

    app update

--- APP/app/src/main/java/vibrationspotter/vibrationspotterapp/MainActivity.java
+++ APP/app/src/main/java/vibrationspotter/vibrationspotterapp/MainActivity.java
@@ -352,16 +352,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             recreate();
 
-        } else if (id == R.id.nav_share) {
+/*        } else if (id == R.id.nav_share) {
             Intent intent = new Intent(Intent.ACTION_PICK);
             intent.setType("image/*");
             startActivityForResult(intent, SELECTED_PIC);
 
         } else if (id == R.id.nav_send) {
 
+        }*/
+
         }
-
-
         return true;
     }
 }
