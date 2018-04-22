12a7e19c847f7af69cb56436b3dc0b938b71a171
Pieterism <pervdh@gmail.com>
2018-04-22 16:34:04 +0200

    map update

--- APP/app/src/main/java/vibrationspotter/vibrationspotterapp/MapActivity.java
+++ APP/app/src/main/java/vibrationspotter/vibrationspotterapp/MapActivity.java
@@ -2,6 +2,7 @@ package vibrationspotter.vibrationspotterapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,9 +11,13 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -23,7 +28,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private boolean mLocationPermissionGranted = false;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private GoogleMap mMap;
-
+    private FusedLocationProviderClient mfusedLocationProviderclient;
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map ready", Toast.LENGTH_LONG).show();
@@ -44,12 +49,35 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(MapActivity.this);
     }
 
+    private void getDeviceLocation(){
+        mfusedLocationProviderclient = LocationServices.getFusedLocationProviderClient(this);
+        try{
+            if(mLocationPermissionGranted){
+                final Task location = mfusedLocationProviderclient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Location currentLocation = (Location) task.getResult();
+                        }else{
+                            Toast.makeText(MapActivity.this, "Unable to get location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        }catch(SecurityException e){
+
+        }
+    }
+
     private void getLocationPermission() {
         String[] permissions = {
                 Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 mLocationPermissionGranted = true;
+                initMap();
             } else {
                 ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
             }
