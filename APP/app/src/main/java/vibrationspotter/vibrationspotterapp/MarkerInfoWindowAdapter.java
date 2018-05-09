36a367ea2d177d10d3a67bcbb1729231fe7087c0
Pieterism <pervdh@gmail.com>
2018-05-09 15:01:30 +0200

    added todo

--- APP/app/src/main/java/vibrationspotter/vibrationspotterapp/MarkerInfoWindowAdapter.java
+++ APP/app/src/main/java/vibrationspotter/vibrationspotterapp/MarkerInfoWindowAdapter.java
@@ -3,6 +3,7 @@ package vibrationspotter.vibrationspotterapp;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,6 +11,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
+//TODO: TextView values uit databank aanvullen
 public class MarkerInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     private Context context;
 
@@ -26,6 +28,8 @@ public class MarkerInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     public View getInfoContents(Marker arg0) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = inflater.inflate(R.layout.map_marker_info_window, null);
+
+
 
         return v;
     }
